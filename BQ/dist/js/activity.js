01cc51ec22a46e151442d55fb03ab3fb830f8c28
Little-Rui <805954138@qq.com>
2018-10-16 23:24:12 +0800

    主页详情页布局结束

--- BQ/dist/js/activity.js
+++ BQ/dist/js/activity.js
@@ -64,8 +64,6 @@ $.extend(WaterFall.prototype,{
         }
         return $.ajax(opt);
     },
-
-    
     renderPage:function(json){
         var html = "";
         for(var i = 0 ; i < json.length ; i ++){
@@ -92,7 +90,6 @@ $.extend(WaterFall.prototype,{
         $(window).on("scroll",this.ifLoad.bind(this));
     },
     ifLoad(){
-       
         var scrollTop = $("html,body").scrollTop();
         var clientHeight = $("html")[0].clientHeight;
         var lastBox = this.main.children(":last");
